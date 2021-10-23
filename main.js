9993b36f90ba28c250912a213fd6771bba55760a
Lucas Bastos <lucas.gbastos@hotmail.com>
2021-10-22 21:05:33 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const linksSocialMedia = {
+  github: 'LGBastos',
+  youtube: '/channel/UCPNA_ITv3xMro1dNA2gUYHQ',
+  facebook: 'lucas.bastos.9803150',
+  instagram: '_lgbastos',
+  twitter: '_lgbastos'
+}
+function changeSocialMediaLinks() {
+  for (let link of socialLinks.children) {
+    const social = link.getAttribute('id')
+
+    link.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+
+    //alert(link.children[0].href)
+  }
+}
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos(userLogin) {
+  const url = `https://api.github.com/users/${userLogin}`
+  fetch(url)
+  .then(response =>  response.json() )
+  .then(data => {
+    userAvatar.src = data.avatar_url
+    userName.textContent = data.name
+    github.href = data.url
+    github.children[1].textContent = data.login
+    userBio.textContent = data.bio;
+   })
+}
+getGitHubProfileInfos(linksSocialMedia.github)
